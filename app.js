279b2425018b5c1e0e5ef55703b0509954c8d855
altanfiz <atthedarkway@gmail.com>
2021-06-19 20:54:26 -0700

    add sumAll function

--- app.js
+++ app.js
@@ -11,4 +11,10 @@ function removeClass(el, name) {
     el.classList.remove(name);
 }
 
+function sumAll(arr) {
+    return arr.reduce((total, current) => {
+        total += current;
+    },0)
+}
+
 sayHi();
